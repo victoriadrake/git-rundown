50bf7d2149aab488846210d773c534de0676ab77
Victorhck <victorhck@mailbox.org>
2020-03-03 17:41:33 +0100

    check language and show localization messages

--- git-rundown.sh
+++ git-rundown.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# Check system language and add strings
+case $LANG in 
+    es_ES.UTF-8)
+        check_modif="modificado"
+        message_modif="modificado"
+        check_untr="seguimiento"
+        message_untr="Archivos sin seguimiento"
+        check_unpush="adelantada"
+        message_unpush="falta hacer push"
+        clean="nada a hacer commit"
+        no_git="no es un repositorio de git"
+        ;;
+    en_EN.UTF-8)
+        check_modif="modified"
+        message_modif="modified"
+        check_untr="Untracked"
+        message_untr="Untracked"
+        check_unpush="ahead"
+        message_unpush"unpushed"
+        clean="nothing to commit"
+        no_git="not a git repository"
+        ;;
+esac
+
+#
+
 dir="$1"
 
 # No directory has been provided, use current
@@ -37,31 +63,31 @@ do
 		echo -en "\033[0;36m${s:10}\033[0m "
 
 		# Check for modified files
-		if [ $(git status | grep modified -c) -ne 0 ]
+		if [ $(git status | grep $check_modif -c) -ne 0 ]
 		then
 			mod=1
-			echo -en "\033[0;93mmodified\033[0m "
+			echo -en "\033[0;93m$message_modif\033[0m "
 		fi
 
 		# Check for untracked files
-		if [ $(git status | grep Untracked -c) -ne 0 ]
+		if [ $(git status | grep $check_untr -c) -ne 0 ]
 		then
 			mod=1
-			echo -en "\033[0;91muntracked\033[0m "
+			echo -en "\033[0;91m$message_untr\033[0m "
 		fi
 
         # Check for unpushed changes
-        if [ $(git status | grep 'Your branch is ahead' -c) -ne 0 ]
+        if [ $(git status | grep $check_unpush -c) -ne 0 ]
         then
             mod=1
-            echo -en "\033[0;92munpushed\033[0m "
+            echo -en "\033[0;92m$message_unpush\033[0m "
         fi
 
 
 		# Check if everything is peachy keen
 		if [ $mod -eq 0 ]
 		then
-			echo -en "nothing to commit"
+			echo -en "$clean"
 		fi
 
         echo -e " "
@@ -70,10 +96,9 @@ do
 	else
 	    echo -en "\033[0;37m"
     	echo "${f}"
-		echo "not a git repository"
+		echo "$no_git"
 	    echo -en "\033[0m"
 	fi
-
-	echo
+    echo
 done
 
